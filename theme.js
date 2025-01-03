f9afa15eeee41f905cbac75d0a613118d020e329
QYLexpired <1525124972@qq.com>
2025-01-03 13:17:06 +0800

    Add files via upload

--- theme.js
+++ theme.js
@@ -85,10 +85,20 @@ function createSettingsWindow() {
     checkbox1.id = 'mark-empty-checkbox';
     checkbox1.checked = isChecked;
 
-    const label1 = document.createElement('label1');
+    const label1 = document.createElement('label');
     label1.htmlFor = 'mark-empty-checkbox';
     label1.textContent = '标记挖空';
     label1.style.fontSize = '14px';
+
+    const checkbox2 = document.createElement('input');
+    checkbox2.type = 'checkbox';
+    checkbox2.id = 'filetree-indent-checkbox';
+    checkbox2.checked = isChecked;
+
+    const label2 = document.createElement('label');
+    label2.htmlFor = 'filetree-indent-checkbox';
+    label2.textContent = '文档树缩进线';
+    label2.style.fontSize = '14px';
 
     // 将复选框和标签组合
     const QYLfunctionpair1 = document.createElement('div');
@@ -96,8 +106,14 @@ function createSettingsWindow() {
     QYLfunctionpair1.appendChild(checkbox1);
     QYLfunctionpair1.appendChild(label1);
 
+    const QYLfunctionpair2 = document.createElement('div');
+    QYLfunctionpair2.className = 'checkbox-label-pair';
+    QYLfunctionpair2.appendChild(checkbox2);
+    QYLfunctionpair2.appendChild(label2);
+
     // 将复选框和标签添加到设置窗口
     settingsWindow.appendChild(QYLfunctionpair1);
+    settingsWindow.appendChild(QYLfunctionpair2);
 
     // 将设置窗口添加到body
     document.body.appendChild(settingsWindow);
@@ -109,6 +125,16 @@ function createSettingsWindow() {
             enableMarkStyles();
         } else {
             disableMarkStyles();
+        }
+    });
+
+    // 文档树缩进线开关
+    checkbox2.addEventListener('change', function() {
+        isChecked = this.checked;
+        if (this.checked) {
+            enableIndentStyle();
+        } else {
+            disableIndentStyle();
         }
     });
 
@@ -164,6 +190,40 @@ function enableMarkStyles() {
 // 关闭标记挖空功能
 function disableMarkStyles() {
     const styleSheet = document.getElementById("mark-styles");
+    if (styleSheet) {
+        styleSheet.innerText = '';
+    }
+}
+
+// 开启文档树缩进线功能
+function enableIndentStyle() {
+    let styleSheet = document.getElementById("indent-style");
+    if (!styleSheet) {
+        styleSheet = document.createElement("style");
+        styleSheet.id = "indent-style";
+        document.head.appendChild(styleSheet);
+    }
+    styleSheet.innerText = `
+        .file-tree>.fn__flex-1 {
+        --indent-color-inactive: rgb(from var(--b3-theme-on-background) r g b / .15);
+        --indent-color-active: rgb(from var(--b3-theme-on-background) r g b / .3);
+        --indent-color: #0000 }
+        .file-tree>.fn__flex-1>ul>ul { background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0 18px, var(--indent-color) 18px 19.5px, rgba(0, 0, 0, 0) 19.5px 100%) }
+        .file-tree>.fn__flex-1>ul>ul>ul { background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0 36px, var(--indent-color) 36px 37.5px, rgba(0, 0, 0, 0) 37.5px 100%) }
+        .file-tree>.fn__flex-1>ul>ul>ul>ul { background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0 54px, var(--indent-color) 54px 55.5px, rgba(0, 0, 0, 0) 55.5px 100%) }
+        .file-tree>.fn__flex-1>ul>ul>ul>ul>ul { background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0 72px, var(--indent-color) 72px 73.5px, rgba(0, 0, 0, 0) 73.5px 100%) }
+        .file-tree>.fn__flex-1>ul>ul>ul>ul>ul>ul { background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0 90px, var(--indent-color) 90px 91.5px, rgba(0, 0, 0, 0) 91.5px 100%) }
+        .file-tree>.fn__flex-1>ul>ul>ul>ul>ul>ul>ul { background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0 108px, var(--indent-color) 108px 109.5px, rgba(0, 0, 0, 0) 109.5px 100%) }
+        .file-tree>.fn__flex-1>ul>ul>ul>ul>ul>ul>ul>ul { background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0 126px, var(--indent-color) 126px 127.5px, rgba(0, 0, 0, 0) 127.5px 100%) }
+        .file-tree>.fn__flex-1>ul ul { transition-duration: .2s; transition-property: --indent-color,height }
+        .file-tree>.fn__flex-1:hover>ul ul,.file-tree>.fn__flex-1 li.b3-list-item--focus+ul,.file-tree>.fn__flex-1 ul.has-focus { --indent-color: var(--b3-theme-background-light) }
+        .file-tree>.fn__flex-1:hover li.b3-list-item--focus+ul,.file-tree>.fn__flex-1:hover ul.has-focus { --indent-color: var(--b3-theme-primary) }
+    `;
+}
+
+// 关闭文档树缩进功能
+function disableIndentStyle() {
+    const styleSheet = document.getElementById("indent-style");
     if (styleSheet) {
         styleSheet.innerText = '';
     }
