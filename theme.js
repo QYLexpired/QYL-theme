9bfefeba66dcb645749f070fe23b7a88876da0c3
QYLexpired <1525124972@qq.com>
2025-01-03 22:48:08 +0800

    Add files via upload

--- theme.js
+++ theme.js
@@ -31,7 +31,7 @@ document.addEventListener('selectionchange', function() {
     addThemeToolBar();
 })();
 
-// Q按钮定义
+// Q按钮定义/Q按钮关闭设置窗口
 function addThemeToolBar() {
     var QYLToolBar = document.getElementById("QToolbar");
     if (!QYLToolBar) {
@@ -62,6 +62,8 @@ function addThemeToolBar() {
 
 // 设置窗口
 let isChecked = false;
+let isChecked2 = false;
+let isChecked3 = false;
 function createSettingsWindow() {
     // 检查是否已经存在设置窗口
     if (document.getElementById('settings-window')) return;
@@ -75,7 +77,7 @@ function createSettingsWindow() {
     settingsWindow.style.backgroundColor = 'rgb(246, 246, 246)';
     settingsWindow.style.padding = '12px';
     settingsWindow.style.border = '1px solid #e0e0e0';
-    settingsWindow.style.boxShadow = '0 8px 24px rgba(0, 0, 0, .2)';
+    settingsWindow.style.boxShadow = 'var(--b3-point-shadow)';
     settingsWindow.style.zIndex = '1000';
     settingsWindow.style.borderRadius = '16px'; 
 
@@ -93,12 +95,22 @@ function createSettingsWindow() {
     const checkbox2 = document.createElement('input');
     checkbox2.type = 'checkbox';
     checkbox2.id = 'filetree-indent-checkbox';
-    checkbox2.checked = isChecked;
+    checkbox2.checked = isChecked2;
 
     const label2 = document.createElement('label');
     label2.htmlFor = 'filetree-indent-checkbox';
     label2.textContent = '文档树缩进线';
     label2.style.fontSize = '14px';
+
+    const checkbox3 = document.createElement('input');
+    checkbox3.type = 'checkbox';
+    checkbox3.id = 'toolbar-hidden-checkbox';
+    checkbox3.checked = isChecked3;
+
+    const label3 = document.createElement('label');
+    label3.htmlFor = 'toolbar-hidden-checkbox';
+    label3.textContent = '隐藏顶栏';
+    label3.style.fontSize = '14px';
 
     // 将复选框和标签组合
     const QYLfunctionpair1 = document.createElement('div');
@@ -111,9 +123,15 @@ function createSettingsWindow() {
     QYLfunctionpair2.appendChild(checkbox2);
     QYLfunctionpair2.appendChild(label2);
 
+    const QYLfunctionpair3 = document.createElement('div');
+    QYLfunctionpair3.className = 'checkbox-label-pair';
+    QYLfunctionpair3.appendChild(checkbox3);
+    QYLfunctionpair3.appendChild(label3);
+
     // 将复选框和标签添加到设置窗口
     settingsWindow.appendChild(QYLfunctionpair1);
     settingsWindow.appendChild(QYLfunctionpair2);
+    settingsWindow.appendChild(QYLfunctionpair3);
 
     // 将设置窗口添加到body
     document.body.appendChild(settingsWindow);
@@ -130,11 +148,21 @@ function createSettingsWindow() {
 
     // 文档树缩进线开关
     checkbox2.addEventListener('change', function() {
-        isChecked = this.checked;
+        isChecked2 = this.checked;
         if (this.checked) {
             enableIndentStyle();
         } else {
             disableIndentStyle();
+        }
+    });
+
+    // 隐藏顶栏开关
+    checkbox3.addEventListener('change', function() {
+        isChecked3 = this.checked;
+        if (this.checked) {
+            enabletoolbarhidden();
+        } else {
+            disabletoolbarhidden();
         }
     });
 
@@ -224,6 +252,41 @@ function enableIndentStyle() {
 // 关闭文档树缩进功能
 function disableIndentStyle() {
     const styleSheet = document.getElementById("indent-style");
+    if (styleSheet) {
+        styleSheet.innerText = '';
+    }
+}
+
+// 开启隐藏顶栏功能
+function enabletoolbarhidden() {
+    let styleSheet = document.getElementById("toolbarhidden-style");
+    if (!styleSheet) {
+        styleSheet = document.createElement("style");
+        styleSheet.id = "toolbarhidden-style";
+        document.head.appendChild(styleSheet);
+    }
+    styleSheet.innerText = `
+        .toolbar {
+            margin-bottom: -32px;
+            opacity: 0;
+            transition: all 200ms;
+            transform: translateY(-30px);
+            z-index: 8;
+            border-bottom-right-radius: var(--b3-border-radius);
+            border-bottom-left-radius: var(--b3-border-radius);
+            box-shadow: var(--b3-point-shadow);
+        }
+        .toolbar:hover {
+            opacity: 1;
+            transform: translateY(0px);
+            transition: all 200ms;
+        }
+    `;
+}
+
+// 关闭隐藏顶栏功能
+function disabletoolbarhidden() {
+    const styleSheet = document.getElementById("toolbarhidden-style");
     if (styleSheet) {
         styleSheet.innerText = '';
     }
