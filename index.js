cf7489e245c24b868ad2f3129c4fa22d6e86d19f
QYLexpired <1525124972@qq.com>
2025-06-30 02:23:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import './script/QYLButton/QYLButton.js';
+import './script/basic/GetStorage.js';
+import './script/QYLSettings/InitQYLSettings.js';
+import './script/basic/StatusPosition.js';
+import './script/basic/StatusHidden.js';
+import './script/basic/ColorPick.js';
---
+++ theme.js
@@ -0,0 +1,5 @@
+import('./index.js')
+    .then(() => {
+    })
+    .catch(error => {
+    });
