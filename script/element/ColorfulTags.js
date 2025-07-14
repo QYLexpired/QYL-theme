d559be2cde9813d5dc635c19e699163f0060fe4a
QYLexpired <1525124972@qq.com>
2025-07-14 18:13:53 +0800

    Add files via upload

--- script/element/ColorfulTags.js
+++ script/element/ColorfulTags.js
@@ -87,6 +87,63 @@ export function initColorfulTags() {
                 background-color: var(--b3-theme-primary);
             }
         }
+        /* 文档标签 */
+        .protyle-background .b3-chips__doctag .b3-chip {
+            border: none !important;
+            outline: none !important;
+            color: var(--b3-theme-on-primary);
+            &:nth-of-type(6n+1) {
+                background-color: rgba(from var(--QYL-custom-red) r g b/0.7);
+                background-color: oklch(calc(0.65 + var(--b3-theme-primary-brightness) * 0.015) calc(0.35 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 120deg));
+            }
+            &:nth-of-type(6n+2) {
+                background-color: rgba(from var(--QYL-custom-orange) r g b/0.7);
+                background-color: oklch(calc(0.65 + var(--b3-theme-primary-brightness) * 0.015) calc(0.35 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 180deg));
+            }
+            &:nth-of-type(6n+3) {
+                background-color: rgba(from var(--QYL-custom-green) r g b/0.7);
+                background-color: oklch(calc(0.65 + var(--b3-theme-primary-brightness) * 0.015) calc(0.35 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 240deg));
+            }
+            &:nth-of-type(6n+4) {
+                background-color: rgba(from var(--QYL-custom-blue) r g b/0.7);
+                background-color: oklch(calc(0.65 + var(--b3-theme-primary-brightness) * 0.015) calc(0.35 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 300deg));
+            }
+            &:nth-of-type(6n+5) {
+                background-color: rgba(from var(--QYL-custom-purple) r g b/0.7);
+                background-color: oklch(calc(0.65 + var(--b3-theme-primary-brightness) * 0.015) calc(0.35 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 360deg));
+            }
+            &:nth-of-type(6n+6) {   
+                background-color: rgba(from var(--QYL-custom-pink) r g b/0.7);
+                background-color: oklch(calc(0.65 + var(--b3-theme-primary-brightness) * 0.015) calc(0.35 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 60deg));
+            }
+        }
+        [data-theme-mode="dark"] .protyle-background .b3-chips__doctag .b3-chip {
+            color: var(--b3-theme-on-primary);
+            &:nth-of-type(6n+1) {
+                background-color: var(--QYL-custom-red);
+                background-color: oklch(calc(0.4 + var(--b3-theme-primary-brightness) * 0.015) calc(0.25 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 120deg));
+            }
+            &:nth-of-type(6n+2) {
+                background-color: var(--QYL-custom-orange);
+                background-color: oklch(calc(0.4 + var(--b3-theme-primary-brightness) * 0.015) calc(0.25 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 180deg));
+            }
+            &:nth-of-type(6n+3) {
+                background-color: var(--QYL-custom-green);
+                background-color: oklch(calc(0.4 + var(--b3-theme-primary-brightness) * 0.015) calc(0.25 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 240deg));
+            }
+            &:nth-of-type(6n+4) {
+                background-color: var(--QYL-custom-blue);
+                background-color: oklch(calc(0.4 + var(--b3-theme-primary-brightness) * 0.015) calc(0.25 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 300deg));
+            }
+            &:nth-of-type(6n+5) {
+                background-color: var(--QYL-custom-purple);
+                background-color: oklch(calc(0.4 + var(--b3-theme-primary-brightness) * 0.015) calc(0.25 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 360deg));
+            }
+            &:nth-of-type(6n+6) {
+                background-color: var(--QYL-custom-pink);
+                background-color: oklch(calc(0.4 + var(--b3-theme-primary-brightness) * 0.015) calc(0.25 * var(--b3-theme-primary-saturate)) calc(var(--b3-theme-primary-main) + 60deg));
+            }
+        }
         /* 行级代码 */
         .fn__code, .b3-typography code:not(.hljs), .b3-typography span[data-type~=code], .protyle-wysiwyg code:not(.hljs), .protyle-wysiwyg span[data-type~=code] {
             &:nth-of-type(6n+1) {
