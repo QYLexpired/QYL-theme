c65b2a1e3eea228a6400a4763f499089f540cd2c
QYLexpired <1525124972@qq.com>
2025-08-12 02:12:47 +0800

    Add files via upload

--- script/QYLAttr/QYLAttrMain.js
+++ script/QYLAttr/QYLAttrMain.js
@@ -9,112 +9,71 @@ class QYLAttr {
         this.api = new QYLAttrAPI();
         this.menu = new QYLAttrMenu(this.i18n, this.api);
         this.isClickMonitorActive = false;
-        this.initTimeout = null;
-        this.insertTimeout = null;
-        this.fileInitTimeout = null;
-        this.fileInsertTimeout = null;
-        this.handleEvent = null;
-        this.touchStartTime = 0;
-        this.touchStartTarget = null;
-        this.touchStartX = 0;
-        this.touchStartY = 0;
-        this.longPressDelay = 500;
-        this.retryCount = 0;
-        this.fileRetryCount = 0; 
+        this.commonMenuObserver = null;
+        this.commonMenuElement = null;
+        this.searchAttempts = 0;
+        this.maxSearchAttempts = 15;
+        this.searchInterval = 100;
+        this.menuShowTimeout = null; 
+        this.isCreatingQYLAttr = false; 
     }
     init() {
         initQYLcustomattrCSS();
         initCustomCSS();
-        this.startClickMonitor();
+        this.startCommonMenuMonitor();
     }
-    startClickMonitor() {
+    startCommonMenuMonitor() {
         if (this.isClickMonitorActive) return;
         this.isClickMonitorActive = true;
-        this.handleBlockEvent = this.debounce((e) => {
-            this.initQYLattr(e);
-        }, 100);
-        this.handleFileEvent = this.debounce((e) => {
-            this.initQYLattrforfile(e);
-        }, 100);
-        this.handleMouseEvent = (e) => {
-            if (
-                e.target.closest('.protyle-gutters') ||
-                e.target.closest('.keyboard__action[data-type="block"]')
-            ) {
-                this.handleBlockEvent(e);
+        this.searchCommonMenu();
+    }
+    searchCommonMenu() {
+        this.commonMenuElement = document.querySelector('#commonMenu');
+        if (this.commonMenuElement) {
+            this.setupCommonMenuObserver();
+        } else {
+            this.searchAttempts++;
+            if (this.searchAttempts < this.maxSearchAttempts) {
+                setTimeout(() => {
+                    this.searchCommonMenu();
+                }, this.searchInterval);
             }
-            if (e.target.closest('.b3-list-item')) {
-                this.handleFileEvent(e);
-            }
-        };
-        this.handleKeyEvent = (e) => {
-            if (e.ctrlKey || e.metaKey) {
-                this.handleBlockEvent(e);
-            }
-        };
-        this.handleTouchStart = (e) => {
-            if (e.touches.length === 1) {
-                const touch = e.touches[0];
-                this.touchStartTime = Date.now();
-                this.touchStartTarget = e.target;
-                this.touchStartX = touch.clientX;
-                this.touchStartY = touch.clientY;
-            }
-        };
-        this.handleTouchEnd = (e) => {
-            if (this.touchStartTime && this.touchStartTarget) {
-                const pressDuration = Date.now() - this.touchStartTime;
-                if (pressDuration >= this.longPressDelay) {
-                    this.handleLongPress(e);
-                } else {
-                    this.handleTouchClick(e);
+        }
+    }
+    setupCommonMenuObserver() {
+        if (this.commonMenuObserver) {
+            this.commonMenuObserver.disconnect();
+        }
+        this.commonMenuObserver = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                    const target = mutation.target;
+                    const oldValue = mutation.oldValue || '';
+                    const newValue = target.className;
+                    const hadFnNone = oldValue.includes('fn__none');
+                    const hasFnNone = newValue.includes('fn__none');
+                    if (hadFnNone && !hasFnNone) {
+                        this.handleCommonMenuShow();
+                    }
                 }
-            }
-            this.touchStartTime = 0;
-            this.touchStartTarget = null;
-            this.touchStartX = 0;
-            this.touchStartY = 0;
-        };
-        this.handleTouchMove = (e) => {
-            if (this.touchStartTarget) {
-                const touch = e.touches[0];
-                const moveDistance = Math.sqrt(
-                    Math.pow(touch.clientX - this.touchStartX, 2) + 
-                    Math.pow(touch.clientY - this.touchStartY, 2)
-                );
-                if (moveDistance > 10) { 
-                    this.touchStartTime = 0;
-                    this.touchStartTarget = null;
-                }
-            }
-        };
-        this.handleLongPress = (e) => {
-            if (
-                e.target.closest('.protyle-gutters') ||
-                e.target.closest('.keyboard__action[data-type="block"]')
-            ) {
-                this.handleBlockEvent(e);
-            }
-            if (e.target.closest('.b3-list-item')) {
-                this.handleFileEvent(e);
-            }
-        };
-        this.handleTouchClick = (e) => {
-            if (
-                e.target.closest('.protyle-gutters') ||
-                e.target.closest('.keyboard__action[data-type="block"]')
-            ) {
-                this.handleBlockEvent(e);
-            }
-            if (e.target.closest('.b3-list-item')) {
-                this.handleFileEvent(e);
-            }
-        };
-        window.addEventListener('mouseup', this.handleMouseEvent);
-        window.addEventListener('keyup', this.handleKeyEvent);
-        window.addEventListener('touchstart', this.handleTouchStart, { passive: false });
-        window.addEventListener('touchend', this.handleTouchEnd, { passive: false });
-        window.addEventListener('touchmove', this.handleTouchMove, { passive: false });
+            });
+        });
+        this.commonMenuObserver.observe(this.commonMenuElement, {
+            attributes: true,
+            attributeFilter: ['class'],
+            attributeOldValue: true
+        });
+    }
+    handleCommonMenuShow() {
+        if (this.isCreatingQYLAttr || document.querySelector('#QYLattr')) {
+            return;
+        }
+        this.isCreatingQYLAttr = true;
+        this.initQYLattr({});
+        this.initQYLattrforfile({});
+        setTimeout(() => {
+            this.isCreatingQYLAttr = false;
+        }, 100);
     }
     debounce(func, delay) {
         let timeout;
@@ -124,66 +83,18 @@ class QYLAttr {
         };
     }
     initQYLattr(e) {
-        clearTimeout(this.initTimeout);
-        clearTimeout(this.insertTimeout);
-        this.retryCount = 0;
-        this.maxRetries = 10;
-        this.retryDelay = 50;
-        const tryInsert = async () => {
-            const selectinfo = this.getBlockSelected();
-            if (selectinfo) {
-                try {
-                    await this.menu.insertQYLattr(selectinfo.id, selectinfo.type, selectinfo.sbLayout);
-                    this.api.queryCSSAttribute(selectinfo.id);
-                    return { success: true, reason: 'inserted' }; 
-                } catch (error) {
-                    return { success: false, reason: 'error' }; 
-                }
-            }
-            return { success: false, reason: 'no-selection' }; 
-        };
-        const attemptInsert = async () => {
-            const result = await tryInsert();
-            if (result.success) {
-                return; 
-            }
-            if (result.reason === 'error' && this.retryCount < this.maxRetries) {
-                this.retryCount++;
-                this.insertTimeout = setTimeout(attemptInsert, this.retryDelay);
-            }
-        };
-        attemptInsert();
+        const selectinfo = this.getBlockSelected();
+        if (selectinfo) {
+            this.menu.insertQYLattr(selectinfo.id, selectinfo.type, selectinfo.sbLayout);
+            this.api.queryCSSAttribute(selectinfo.id);
+        }
     }
     initQYLattrforfile() {
-        clearTimeout(this.fileInitTimeout);
-        clearTimeout(this.fileInsertTimeout);
-        this.fileRetryCount = 0;
-        this.fileMaxRetries = 10;
-        this.fileRetryDelay = 50;
-        const tryInsertFile = async () => {
-            const selectinfo = this.getFileBlockSelected();
-            if (selectinfo) {
-                try {
-                    await this.menu.insertQYLattrforfile(selectinfo.id, selectinfo.type);
-                    this.api.queryCSSAttribute(selectinfo.id);
-                    return { success: true, reason: 'inserted' }; 
-                } catch (error) {
-                    return { success: false, reason: 'error' }; 
-                }
-            }
-            return { success: false, reason: 'no-selection' }; 
-        };
-        const attemptFileInsert = async () => {
-            const result = await tryInsertFile();
-            if (result.success) {
-                return; 
-            }
-            if (result.reason === 'error' && this.fileRetryCount < this.fileMaxRetries) {
-                this.fileRetryCount++;
-                this.fileInsertTimeout = setTimeout(attemptFileInsert, this.fileRetryDelay);
-            }
-        };
-        attemptFileInsert();
+        const selectinfo = this.getFileBlockSelected();
+        if (selectinfo) {
+            this.menu.insertQYLattrforfile(selectinfo.id, selectinfo.type);
+            this.api.queryCSSAttribute(selectinfo.id);
+        }
     }
     getBlockSelected() {
         const node_list = document.querySelectorAll('.protyle-wysiwyg--select');
@@ -212,32 +123,18 @@ class QYLAttr {
         cleanupQYLcustomattrCSS();
         cleanupCustomCSS();
         if (this.isClickMonitorActive) {
-            window.removeEventListener('mouseup', this.handleMouseEvent);
-            window.removeEventListener('keyup', this.handleKeyEvent);
-            window.removeEventListener('touchstart', this.handleTouchStart);
-            window.removeEventListener('touchend', this.handleTouchEnd);
-            window.removeEventListener('touchmove', this.handleTouchMove);
+            if (this.commonMenuObserver) {
+                this.commonMenuObserver.disconnect();
+                this.commonMenuObserver = null;
+            }
             this.isClickMonitorActive = false;
             this.handleBlockEvent = null;
             this.handleFileEvent = null;
-            this.handleMouseEvent = null;
-            this.handleKeyEvent = null;
-            this.handleTouchStart = null;
-            this.handleTouchEnd = null;
-            this.handleTouchMove = null;
-            this.handleLongPress = null;
-            this.handleTouchClick = null;
         }
-        this.touchStartTime = 0;
-        this.touchStartTarget = null;
-        this.touchStartX = 0;
-        this.touchStartY = 0;
-        this.retryCount = 0;
-        this.fileRetryCount = 0;
-        clearTimeout(this.initTimeout);
-        clearTimeout(this.insertTimeout);
-        clearTimeout(this.fileInitTimeout);
-        clearTimeout(this.fileInsertTimeout);
+        this.commonMenuElement = null;
+        this.searchAttempts = 0;
+        this.isCreatingQYLAttr = false;
+        clearTimeout(this.menuShowTimeout);
         this.cleanupMenuElements();
         if (this.api) {
             this.api.commonMenuCache = { elem: null, timestamp: 0 };
