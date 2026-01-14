59e72eee9fc951b5c0fe13eb514fce6306757f6d
QYLexpired <1525124972@qq.com>
2026-01-14 09:46:55 +0800

    Add files via upload

--- script/basic/Device.js
+++ script/basic/Device.js
@@ -2,3 +2,7 @@ export const isMobile = !!window.siyuan?.mobile;
 if (isMobile) {
   document.body.classList.add('QYLmobile');
 }
+const isMac = navigator.userAgent.includes('Mac') && navigator.userAgent.includes('Electron');
+if (isMac) {
+  document.body.classList.add('QYLmac');
+}
